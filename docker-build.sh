abd59a8fb7e897eb0e0220d69412b1a5145b9fb8
Noel Espino Córdova <noelespinocordova@tutanota.com>
2022-03-17 18:33:36 -0600

    Delete docker-build.sh

--- docker-build.sh
+++
@@ -1,2 +0,0 @@
-docker build . --tag slingercode-drive-client:$(git log -1 --format=%h) &&
-  docker run --name slingercode-drive-client -d -p 80:80 slingercode-drive-client:$(git log -1 --format=%h)
