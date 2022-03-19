b65fcc0c92d10cf677d2943d9c00418972841da2
Noel Espino Córdova <noelespinocordova@tutanota.com>
2022-03-19 17:04:09 -0600

    Fetch image from server

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
   const [connection, setConnection] = useState(false);
+  const [image, setImage] = useState(null);
 
   useEffect(() => {
     const getConnection = async () => {
@@ -14,10 +15,34 @@ const Home = () => {
     getConnection();
   }, []);
 
+  useEffect(() => {
+    const getImage = async () => {
+      const serverUrl = process.env.REACT_APP_SERVER_URL;
+      const response = await axios.get(`${serverUrl}/aws`);
+      setImage(response.data);
+    };
+  
+    getImage();
+  }, []);
+  
+
   return (
     <div className="flex flex-col">
       <div>
         {`slingercode's cloud: Server connection ${connection}`}
+      </div>
+
+
+      <div className="flex flex-col items-center">
+        Image
+
+        {image && (
+          <img
+            alt="from aws"
+            src={`data:image/png;base64,${image}`}
+            className="mt-5 w-8/12"
+          />
+        )}
       </div>
     </div>
   );
